b3065bdab2277aacff6e81638c6f0ce49152eb93
vovka <lesnikov.vladimir@gmail.com>
2023-08-21 12:19:43 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    addCustomerAction,
+    removeCustomerAction,
+} from './store/customerReducer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch();
+    const cash = useSelector((state) => state.cash.cash);
+    const customers = useSelector((state) => state.customers.customers);
+    console.log('cash', cash);
+
+    const addCash = (cash) => {
+        dispatch({ type: 'ADD_CASH', payload: cash });
+    };
+
+    const removeCash = (cash) => {
+        dispatch({ type: 'REMOVE_CASH', payload: cash });
+    };
+
+    const addCustomer = (name) => {
+        const customer = {
+            name: name,
+            id: Date.now(),
+        };
+        dispatch(addCustomerAction(customer));
+    };
+
+    const removeCustomer = (customer) => {
+        dispatch(removeCustomerAction(customer.id));
+    };
+
+    return (
+        <div className="App">
+            <div>{cash}</div>
+            <div style={{ display: 'flex' }}>
+                <button onClick={() => addCash(Number(prompt('Add Cash')))}>
+                    Add Cash
+                </button>
+                <button
+                    onClick={() => removeCash(Number(prompt('Remove Cash')))}
+                >
+                    Remove Cash
+                </button>
+                <button onClick={() => addCustomer(prompt('Add Customer'))}>
+                    Add Customer
+                </button>
+                <button
+                    onClick={() => removeCash(Number(prompt('Remove Client')))}
+                >
+                    Remove Cash
+                </button>
+            </div>
+            {customers.length > 0 ? (
+                <div>
+                    {customers.map((customer) => (
+                        <div
+                            onClick={() => removeCustomer(customer)}
+                            style={{ border: '1px solid red', padding: '30px' }}
+                        >
+                            {customer.name}
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div>No customers</div>
+            )}
+        </div>
+    );
 }
 
 export default App;
